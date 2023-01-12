5c88966abcda9c1a15e1a70cfc483a4be4e6e4b8
osipovmr <osipovmr@yandex.ru>
2023-01-13 01:50:24 +0600

    MVP

---
+++ src/main/java/vaintell/TelegramBot/config/BotInitializer.java
@@ -0,0 +1,28 @@
+package vaintell.TelegramBot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import vaintell.TelegramBot.service.TelegramBot;
+
+@Component
+public class BotInitializer {
+
+    @Autowired
+    TelegramBot telegramBot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(telegramBot);
+        }
+        catch (TelegramApiException e) {
+            throw new RuntimeException();
+        }
+    }
+}
---
+++ src/main/java/vaintell/TelegramBot/service/TelegramBot.java
@@ -0,0 +1,55 @@
+package vaintell.TelegramBot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import vaintell.TelegramBot.config.BotConfig;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Autowired
+    final BotConfig botConfig;
+
+    public TelegramBot(BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendTextMessage(chatId, "Sorry, command was not recognized.");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Hello, " + name + "! Nice to meet you!";
+        sendTextMessage(chatId, answer);
+    }
+
+    private void sendTextMessage(long chatId, String textToSend) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+    }
+}
