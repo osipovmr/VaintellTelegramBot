cda0230cb128d4f11c1b99b504bb46caea452c27
osipovmr <osipovmr@yandex.ru>
2023-01-14 21:54:47 +0600

    debug

--- src/main/java/vaintell/TelegramBot/TelegramBotApplication.java
+++ src/main/java/vaintell/TelegramBot/TelegramBotApplication.java
@@ -3,11 +3,6 @@ package vaintell.TelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-
 @SpringBootApplication
 public class TelegramBotApplication {
 
--- src/main/java/vaintell/TelegramBot/controller/Controller.java
+++ src/main/java/vaintell/TelegramBot/controller/Controller.java
@@ -1,8 +1,6 @@
 package vaintell.TelegramBot.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
